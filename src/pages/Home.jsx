2a024c5313b58b34627387bc21b13895a4a820c8
Alfred Chin <alfredchin2005@gmail.com>
2023-06-15 15:28:09 +0800

    Display only login user todos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,31 @@ import { useContext } from "react"
 import { TodoContext } from "../contexts/TodoContext"
 import { Container, Row, Col } from "react-bootstrap"
 import TodoCard from "../components/TodoCard"
+import AuthContext from "../contexts/AuthContext"
 
-function CardGroup( { todos }) {
+function CardGroup( { todos, loginStatus }) {
     return todos.map((todo) => {
+      if (loginStatus === todo.userId) {
         return (
+          
             <Col md={4} key={todo.id}>
                 <TodoCard todo={todo} />
             </Col>
+
+          
         )
+      }
     })
 }
 
 export default function Home() {
     const todos = useContext(TodoContext).todos
+    const loginStatus = useContext(AuthContext).loginStatus
   return (
     <Container>
       <h1 className="my-3">Your todos</h1>
       <Row>
-        <CardGroup todos={todos} />
+        <CardGroup todos={todos} loginStatus={loginStatus} />
       </Row>
     </Container>
   )
